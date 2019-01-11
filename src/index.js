13718822d1e9c90196c092686139a5129b364b57
Stephen Bavier <sbavier@nygenome.org>
2019-01-11 15:45:25 -0500

    added initiation of redux, sagas, redux-saga Provider, history and router

--- src/index.js
+++ src/index.js
@@ -1,10 +1,62 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+// add bootstrap (optional) prior to index.css
+
+// apply styles
 import './index.css';
+
+// Import components here
 import App from './App';
+
+// creating with middleware, redux and router
+import { createStore, applyMiddleware, compose } from 'redux'
+import { Provider } from 'react-redux'
+import createSagaMiddleware from 'redux-saga'
+import { Router, Route, BrowserRouter, Switch, Redirect } from 'react-router-dom'
+import history from './components/history/history'
+
+// import combined reducer and saga
+import IndexReducer from './index-reducer'
+import IndexSagas from './index-sagas'
+
+// service worker
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// initiate sagaMiddleware
+const sagaMiddleware = createSagaMiddleware()
+
+// eslint disable
+const composeSetup = process.env.NODE_ENV !== 'production' && typeof window === 'object' &&
+   window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+   window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : compose
+
+// create store and the combined reducers from IndexReducer
+const store = createStore(
+   IndexReducer,
+   composeSetup( applyMiddleware( sagaMiddleware ) )
+)
+
+// Initiate Index Saga
+sagaMiddleware.run( IndexSagas )
+
+// setup top level router for React Router
+
+let root = document.getElementById('root')
+
+ReactDOM.render(
+   <Provider store={ store }>
+      <BrowserRouter>
+         <Router history={ history }>
+            <Switch>
+               <Route path="/" component={ App } />
+               <Redirect to="/" />
+            </Switch>
+         </Router>
+      </BrowserRouter>
+   </Provider>, 
+   root
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
